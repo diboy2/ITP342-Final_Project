3ab249fabe53263e3dc63d673f722116eb2b7bf3
ITP Student <itp@ohe540pc11.itplab.usc.edu>
2015-05-01 16:45:54 -0700

    Changed something

--- Class_Check_In/CheckedInStudentsTableViewController.m
+++ Class_Check_In/CheckedInStudentsTableViewController.m
@@ -43,15 +43,16 @@
     return 0;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    static NSString *cellIdentifier = @"checkedInStudentCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
     
     // Configure the cell...
     
     return cell;
 }
-*/
+
 
 /*
 // Override to support conditional editing of the table view.
